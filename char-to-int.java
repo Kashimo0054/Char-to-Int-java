231760dce3d6f3ea14d889cc6330ee95d51dccd0
Sumeet <126050945+Kashimo0054@users.noreply.github.com>
2025-02-03 16:46:05 +0530

    Create char-to-int.java

---
+++ char-to-int.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        System.out.println("Enter your chracter: ");
+
+        char a = '3';
+
+        System.out.println("Your character is: " + a);
+
+        int b = Integer.parseInt(String.valueOf(a));
+
+        System.out.println("Your character in integer is: " + b);
+
+    }
+}
